62c7059f36c7a57e5a8c51cea457c88e08a7c6ab
Pahlo <contactoemail.pablo@gmail.com>
2023-05-12 10:49:51 +0200

    HomeScreen Cards 1.0

--- components/Organisms/NavOptions.js
+++ components/Organisms/NavOptions.js
@@ -1,11 +1,45 @@
 import React from "react";
-import { SafeAreaView, Text, View, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { FlatList } from "react-native";
 
+const data = [
+    {
+        id: "1",
+        title: "Get a Ride",
+        image: "https://links.papareact.com/3pn",
+        screen: "MapScreen"
+    },
+    {
+        id: "2",
+        title: "Order Food",
+        image: "https://links.papareact.com/28w",
+        screen: "EatsScreen"
+    },
+]
 const NavOptions = () => {
     return(
-            <View className="p-5 bg-green-400">
-                <Text>Nav</Text>
-            </View>
+        <FlatList 
+        data={data}
+        horizontal
+        renderItem={({item}) => (
+            <TouchableOpacity>
+                <View className="bg-gray-200 flex items-center justify-center p-2 pl-6 pb-8 pt-4">
+                    <Image
+                    style={{
+                        width:120,
+                        height: 120,
+                        resizeMode: "contain"
+                    }}
+                    source={{
+                        uri: item.image
+                    }}
+                    />
+                    <Text>{item.title}</Text>
+                </View>
+         
+            </TouchableOpacity>
+        )}
+        />
     );
 }
 
